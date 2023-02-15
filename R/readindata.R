b28cbf41d31654b7725313f1d23d96c0232fdaf3
aylit <49897888+aylit@users.noreply.github.com>
2023-02-15 16:49:11 +0100

    Update readindata.R

--- R/readindata.R
+++ R/readindata.R
@@ -1,6 +1,8 @@
 data <- read.csv("sSBR9_rep_1_output_sample.csv", header=TRUE, sep = ",")
 data <- filter(data, step == 41) # filtering for timestep 41
 
+source("nullmodel.R")
+
 get_sSBR_dist(dataset = data, frag_level_low = 0.1, frag_level_high = 0.9, 
               sample_size = 100)
 get_null_model(dataset = data, frag_level_low = 0.1, frag_level_high = 0.9, 
